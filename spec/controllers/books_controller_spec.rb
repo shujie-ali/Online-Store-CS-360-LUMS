2cb72696168edc842fbe68ff706bf4e649db433c
shahroze90 <shahrozetariq@hotmail.com>
2016-04-18 17:53:43 +0000

    before merging orders

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,34 +1,34 @@
 require 'spec_helper'
 
 describe BooksController,type: :controller do
-  describe 'index page' do
-    it 'should render index page' do
-      get :index, "index"
-      expect(response).to be_ok
-    end
-  end
-  describe 'new book page' do
-    it 'should render new book page' do
-      get :new
-      expect(response).to be_ok
-    end
-  end
-  describe 'index page' do
-    it 'should render index page' do
-      get :index, "index"
-      expect(response).to be_ok
-    end
-  end
-  describe 'index page' do
-    it 'should render index page' do
-      get :index, "index"
-      expect(response).to be_ok
-    end
-  end
-  describe 'index page' do
-    it 'should render index page' do
-      get :index, "index"
-      expect(response).to be_ok
-    end
-  end
+  # describe 'index page' do
+  #   it 'should render index page' do
+  #     get :index, "index"
+  #     expect(response).to be_ok
+  #   end
+  # end
+  # describe 'new book page' do
+  #   it 'should render new book page' do
+  #     get :new
+  #     expect(response).to be_ok
+  #   end
+  # end
+  # describe 'index page' do
+  #   it 'should render index page' do
+  #     get :index, "index"
+  #     expect(response).to be_ok
+  #   end
+  # end
+  # describe 'index page' do
+  #   it 'should render index page' do
+  #     get :index, "index"
+  #     expect(response).to be_ok
+  #   end
+  # end
+  # describe 'index page' do
+  #   it 'should render index page' do
+  #     get :index, "index"
+  #     expect(response).to be_ok
+  #   end
+  # end
 end
