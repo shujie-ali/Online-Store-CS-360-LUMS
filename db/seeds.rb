6929b83a927e3cf77d1f87e7beb0a302a18e21e6
shahrozpunjwani <shahrozpunjwani@gmail.com>
2016-05-02 11:38:37 +0000

    modificationEmail

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,4 @@ def digest(string)
   BCrypt::Password.create(string, cost: cost)
 end    
     
-admin = Admin.create! :first_name => 'admin', :last_name => 'admin', :email => 'admin@onepoint.com', :password_digest => digest('admin')
+Admin.create(first_name: 'admin', last_name: 'admin', email: 'admin@onepoint.com', password_digest: digest('shahroz') ) 
